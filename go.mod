89b2b3068ba8499bc2bc0afd6040892c4d4651cd
Kota117 <dakotah1375@gmail.com>
2026-05-12 21:08:35 -0600

    Added config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kota117/blog_aggregator
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Kota117/blog_aggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("dakota")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
