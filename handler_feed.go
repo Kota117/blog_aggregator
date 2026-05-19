93b147c298a653a2f18a88077ce7b2123296f301
Kota117 <dakotah1375@gmail.com>
2026-05-18 18:28:21 -0600

    Added ability to list all feeds

--- handler_feed.go
+++ handler_feed.go
@@ -30,14 +30,46 @@ func handlerAddFeed(s *state, cmd command) error {
 		UserID:    current_user.ID,
 	})
 	if err != nil {
-		return err
+		return fmt.Errorf("Couldn't create feed: %w", err)
 	}
 
+	fmt.Println("Feed created successfully:")
+	printFeed(feed, current_user)
+	fmt.Println()
+	fmt.Println("=====================================")
+
+	return nil
+}
+
+func printFeed(feed database.Feed, user database.User) {
 	fmt.Printf(" * ID: %v\n", feed.ID)
 	fmt.Printf(" * CreatedAt: %v\n", feed.CreatedAt)
 	fmt.Printf(" * UpdatedAt: %v\n", feed.UpdatedAt)
 	fmt.Printf(" * Name: %v\n", feed.Name)
 	fmt.Printf(" * Url: %v\n", feed.Url)
-	fmt.Printf(" * UserID: %v\n", feed.UserID)
+	fmt.Printf(" * User: %v\n", user.Name)
+}
+
+func handlerListFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("Couldn't list feeds: %w", err)
+	}
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
+	for _, feed := range feeds {
+		user, err := s.db.GetUserById(context.Background(), feed.UserID)
+		if err != nil {
+			return fmt.Errorf("Couldn't get user: %w", err)
+		}
+		printFeed(feed, user)
+		fmt.Println()
+		fmt.Println("=====================================")
+	}
 	return nil
 }
